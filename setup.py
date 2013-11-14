84664038463ac38d44ee61ced33aca3039435aeb
Wolfgang Schnerring <ws@gocept.com>
2013-11-14 15:43:58 +0100

    Metadata

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
-# Copyright (c) 2013 gocept gmbh & co. kg
-# See also LICENSE.txt
-
 # This should be only one line. If it must be multi-line, indent the second
 # line onwards to keep the PKG-INFO file format intact.
-"""
+"""Disable SSL verification of setuptools for buildout
 """
 
 from setuptools import setup, find_packages
@@ -29,8 +26,8 @@ setup(
         ],
     },
 
-    author='gocept <mail@gocept.com>',
-    author_email='mail@gocept.com',
+    author='Wolfgang Schnerring <ws@gocept.com>',
+    author_email='ws@gocept.com',
     license='ZPL 2.1',
     url='https://bitbucket.org/gocept/buildout.disablessl/',
 
